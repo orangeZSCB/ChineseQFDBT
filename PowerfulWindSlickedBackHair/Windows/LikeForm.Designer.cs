7bf039d6ded02c3c65fb7a6a746bad8f485ecc40
orangeZSCB <liailio3@outlook.com>
2023-07-01 19:20:50 +0800

    ALL UPDATED

---
+++ PowerfulWindSlickedBackHair/Windows/LikeForm.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace PowerfulWindSlickedBackHair.Windows
+{
+	// Token: 0x02000012 RID: 18
+	public partial class LikeForm : global::System.Windows.Forms.Form
+	{
+		// Token: 0x06000063 RID: 99 RVA: 0x00006620 File Offset: 0x00004820
+		protected override void Dispose(bool disposing)
+		{
+			bool flag = disposing && this.components != null;
+			if (flag)
+			{
+				this.components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		// Token: 0x06000064 RID: 100 RVA: 0x00006658 File Offset: 0x00004858
+		private void InitializeComponent()
+		{
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LikeForm));
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.BackgroundImage")));
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox2.Image = global::PowerfulWindSlickedBackHair.Properties.Resources.TheEndSTbg;
+            this.pictureBox2.Location = new System.Drawing.Point(88, 11);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(557, 450);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // LikeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(216)))), ((int)(((byte)(81)))));
+            this.ClientSize = new System.Drawing.Size(716, 468);
+            this.Controls.Add(this.pictureBox2);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "LikeForm";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "一键三连！";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+		}
+
+		// Token: 0x04000041 RID: 65
+		private global::System.ComponentModel.IContainer components = null;
+
+		// Token: 0x04000043 RID: 67
+		private global::System.Windows.Forms.PictureBox pictureBox2;
+	}
+}
---
+++ README.md
@@ -0,0 +1,30 @@
+# PowerfulWindSlickedBackHair CS-LX Improve
+## 强  风  大  窗  口
+
+b站刷到的视频，感觉有意思，程序原作者说源码不小心搞丢了，好在可以反编译，这是反编译完成的项目
+
+除了反编译还进行了以下改进：
+
+#### 1、字体加入资源文件，解决系统没有安装相应字体，导致字幕变成默认宋体
+#### 2、删除exe.config，只要有安装framework 4.7.2或以上就能正常运行，一般win10或以上自带不用再安装
+#### 3、打开程序时，自动最小化其他窗口
+#### 4、现在整个桌面也会随背景变色，效果更好（开了第三方壁纸软件可能变不了）
+
+还有一些其他小变动。
+如果音画不同步，可以调offset.txt的数值，第一个数值默认17，数值越大窗口速率越快，第二个数值默认0，调整声音提前播放毫秒数。
+
+### 使用方法
+
+>有些来访问的观众第一次用GitHub，这里简单介绍一下如何使用
+>1. 点绿色按钮Code，再点download zip下载，这个步骤所有github项目通用
+>
+>![image](https://github.com/SunnyDesignor/PowerfulWindSlickedBackHairCS-LX_Improve/assets/50539661/db85914c-2f22-4f28-bd46-24406e6507eb)
+>
+>2. 解压下载完的zip，找到bin/Debug/目录下的exe双击运行即可开启狂风
+>
+
+B站视频：https://www.bilibili.com/video/BV1Kz4y1p7sz
+
+原作者：https://github.com/CS-LX
+
+反编译重建/改进：https://space.bilibili.com/286746249
